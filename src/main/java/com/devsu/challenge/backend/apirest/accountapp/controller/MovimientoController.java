8b8f03d9de2449f402de8b6d9cc1e10cf63f78d3
Yvan Lopez <ylopez@getontop.com>
2024-05-29 02:37:38 -0500

    dominio movimiento hecho. Listo para probar.

--- src/main/java/com/devsu/challenge/backend/apirest/accountapp/controller/MovimientoController.java
+++ src/main/java/com/devsu/challenge/backend/apirest/accountapp/controller/MovimientoController.java
@@ -1,9 +1,12 @@
 package com.devsu.challenge.backend.apirest.accountapp.controller;
 
 import com.devsu.challenge.backend.apirest.accountapp.entity.Movimiento;
+import com.devsu.challenge.backend.apirest.accountapp.exceptions.CuentaNoExisteException;
 import com.devsu.challenge.backend.apirest.accountapp.service.IMovimientoService;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,28 +27,55 @@ public class MovimientoController {
     }
 
     @GetMapping
-    public List<Movimiento> getAllMovimientos() {
-        return movimientoService.getAllMovimientos();
+    public ResponseEntity<List<Movimiento>> getAllMovimientos() {
+        List<Movimiento> movimientos = movimientoService.getAllMovimientos();
+        if (movimientos.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(movimientos, HttpStatus.OK);
     }
 
     @PostMapping
-    public Movimiento createMovimiento(@RequestBody Movimiento movimiento) {
-        return movimientoService.createMovimiento(movimiento);
+    public ResponseEntity<Movimiento> crearMovimiento(@RequestBody Movimiento movimiento) {
+        try {
+            Movimiento nuevoMov = movimientoService.createMovimiento(movimiento);
+            return new ResponseEntity<>(nuevoMov, HttpStatus.CREATED);
+        } catch (CuentaNoExisteException ex) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
     }
 
     @GetMapping("/{id}")
-    public Optional<Movimiento> getMovimientoById(@PathVariable Long id) {
-        return movimientoService.getMovimientoById(id);
+    public ResponseEntity<Movimiento> getMovimientoById(@PathVariable Long id) {
+        Optional<Movimiento> movimiento = movimientoService.getMovimientoById(id);
+        return movimiento
+            .map(mov -> new ResponseEntity<>(mov, HttpStatus.OK))
+            .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @PutMapping("/{id}")
-    public Movimiento updateMovimiento(@PathVariable Long id, @RequestBody Movimiento movimientoDetails) {
-        return movimientoService.updateMovimiento(id, movimientoDetails);
+    public ResponseEntity<Movimiento> updateMovimiento(@PathVariable Long id, @RequestBody Movimiento movimientoDetalle) {
+        Movimiento updatedMov = movimientoService.updateMovimiento(id, movimientoDetalle);
+        if (updatedMov == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(updatedMov, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteMovimiento(@PathVariable Long id) {
-        movimientoService.deleteMovimiento(id);
+    public ResponseEntity<Void> deleteMovimiento(@PathVariable Long id) {
+        Optional<Movimiento> movimiento = movimientoService.getMovimientoById(id);
+
+        if (movimiento.isPresent()) {
+            movimientoService.deleteMovimiento(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
     }
 
 }
--- src/main/java/com/devsu/challenge/backend/apirest/accountapp/service/impl/MovimientoServiceImpl.java
+++ src/main/java/com/devsu/challenge/backend/apirest/accountapp/service/impl/MovimientoServiceImpl.java
@@ -1,38 +1,62 @@
 package com.devsu.challenge.backend.apirest.accountapp.service.impl;
 
+import com.devsu.challenge.backend.apirest.accountapp.entity.Cuenta;
 import com.devsu.challenge.backend.apirest.accountapp.entity.Movimiento;
+import com.devsu.challenge.backend.apirest.accountapp.exceptions.CuentaNoExisteException;
+import com.devsu.challenge.backend.apirest.accountapp.repository.CuentaRepository;
 import com.devsu.challenge.backend.apirest.accountapp.repository.MovimientoRepository;
 import com.devsu.challenge.backend.apirest.accountapp.service.IMovimientoService;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class MovimientoServiceImpl implements IMovimientoService {
 
     private final MovimientoRepository movimientoRepository;
+    private final CuentaRepository cuentaRepository;
 
-    public MovimientoServiceImpl(MovimientoRepository movimientoRepository) {
+    public MovimientoServiceImpl(MovimientoRepository movimientoRepository, CuentaRepository cuentaRepository) {
         this.movimientoRepository = movimientoRepository;
+        this.cuentaRepository = cuentaRepository;
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<Movimiento> getAllMovimientos() {
         return movimientoRepository.findAll();
     }
 
     @Override
+    @Transactional(readOnly = true)
     public Optional<Movimiento> getMovimientoById(Long id) {
         return movimientoRepository.findById(id);
     }
 
     @Override
     public Movimiento createMovimiento(Movimiento movimiento) {
-        return movimientoRepository.save(movimiento);
+
+        // Verificar si la cuenta existe y está activa
+        Optional<Cuenta> cuentaOpt = cuentaRepository.findByNumeroCuenta(movimiento.getCuenta().getNumeroCuenta());
+
+        if (cuentaOpt.isPresent() && cuentaOpt.get().isEstado()) {
+            return movimientoRepository.save(movimiento);
+        } else {
+            throw new CuentaNoExisteException("La cuenta no existe o no está activa");
+        }
     }
 
     @Override
     public Movimiento updateMovimiento(Long id, Movimiento detalleMovimiento) {
+
+        // Verificar si la cuenta existe y está activa
+        Optional<Cuenta> cuentaOpt = cuentaRepository.findByNumeroCuenta(detalleMovimiento.getCuenta().getNumeroCuenta());
+
+        if (cuentaOpt.isEmpty() || !cuentaOpt.get().isEstado()) {
+            throw new CuentaNoExisteException("La cuenta no existe o no está activa");
+        }
+
         return movimientoRepository.findById(id)
             .map(movimiento -> {
                 movimiento.setFecha(detalleMovimiento.getFecha());
